284190ab4d80f8ee78e14141f044cb0d1469d4f3
FritzFlorian <flo.fritz@t-online.de>
2018-02-06 10:55:13 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hometrainer',
+      version='0.1',
+      description='Simple AlhpaZero implementation',
+      url='https://github.com/FritzFlorian/hometrainer',
+      author='Fritz Florian',
+      license='MIT',
+      packages=['hometrainer'],
+      zip_safe=False)
