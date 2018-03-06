b08b9917f71701a7cae6bbac966531237dd6d2b6
FritzFlorian <flo.fritz@t-online.de>
2018-03-06 17:44:21 +0100

    Bump Version to 0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hometrainer',
-      version='0.1.1',
+      version='0.1.2',
       description='Simple AlphaZero implementation',
       url='https://github.com/FritzFlorian/hometrainer',
       author='Fritz Florian',
